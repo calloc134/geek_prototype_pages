2d037f3356418e0c2ee96a80521c726089dbc2ee
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-11-16 15:52:15 +0900

    chore: スタイルを整えました

--- src/_document.tsx
+++ src/_document.tsx
@@ -1,16 +1,30 @@
 import { Outlet } from "@tanstack/react-router";
 import { css } from "./lib/styled-system/css";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Camera } from "tabler-icons-react";
 
 const Document = () => {
   return (
     <>
       <div
         className={css({
-          backgroundColor: "ruby.dark.a3",
+          padding: 4,
+          bg: "gray.3",
+          display: "flex",
+          alignItems: "center",
+          gap: 4,
         })}
       >
-        AAAAAAAAAAAAAAAA
+        <Camera size={32} color={"black"} className={css({})} />
+        <h1
+          className={css({
+            fontSize: 24,
+            color: "gray.dark.1",
+            fontFamily: "Noto Sans JP",
+          })}
+        >
+          あいろうあ
+        </h1>
         <Outlet />
       </div>
       <TanStackRouterDevtools />
