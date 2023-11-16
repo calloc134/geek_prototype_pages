a6f3ce2c68436a77adcfaa1e97c0925d0528e099
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-11-16 13:16:30 +0000

    style: 404ページの実装

--- src/_document.tsx
+++ src/_document.tsx
@@ -11,6 +11,7 @@ const Document = () => {
         display: "flex",
         height: "full",
         width: "full",
+        minHeight: "full",
         bg: "gray.2",
       })}
     >
--- src/route.tsx
+++ src/route.tsx
@@ -1,6 +1,7 @@
 import { RootRoute, Route, Router } from "@tanstack/react-router";
 import { Document } from "./_document";
 import { Index } from "./pages/index/Index";
+import { NotFound } from "./pages/index/NotFound";
 
 const root_route = new RootRoute({
   component: () => <Document />,
@@ -12,8 +13,14 @@ const index_route = new Route({
   component: () => <Index />,
 });
 
+const not_found_route = new Route({
+  getParentRoute: () => root_route,
+  path: "*",
+  component: () => <NotFound />,
+});
+
 const router = new Router({
-  routeTree: root_route.addChildren([index_route]),
+  routeTree: root_route.addChildren([index_route, not_found_route]),
 });
 
 // tanstack routerを型安全に利用するための型定義
